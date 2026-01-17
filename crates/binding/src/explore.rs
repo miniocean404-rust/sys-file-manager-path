e31159c174fb4f5321bf6127860fe35ffb7c96ba
MiniOcean404 <1037306928@qq.com>
2026-01-17 21:43:23 +0800

    ♻️ refactor: 优化 napi

--- crates/binding/src/explore.rs
+++ crates/binding/src/explore.rs
@@ -1,4 +1,4 @@
-use napi::{Error, Status};
+use napi::bindgen_prelude::*;
 use napi_derive::napi;
 
 use explore::dto::app_info::Platform as SDKPlatform;
@@ -32,53 +32,39 @@ pub struct AppInfo {
 
     // #[napi(ts_type = "object")]
     // pub type_override_optional: Option<String>,
-
-    // 句柄
+    /// 句柄
     pub hwnd_id: u32,
-
-    // 窗口标题
-    #[napi(ts_type = "string")]
+    /// 窗口标题
     pub title: String,
-
-    #[napi(ts_type = "string")]
-    // MacOS bundleId
+    /// MacOS bundleId
     pub bundle_id: String,
-
-    #[napi(ts_type = "boolean")]
-    // 是否激活
+    /// 是否激活
     pub is_active: bool,
-
-    #[napi(ts_type = "string")]
-    // 目录路径
+    /// 目录路径
     pub dir: String,
-
-    #[napi(ts_type = "string")]
-    // 执行程序路径
+    /// 执行程序路径
     pub exec: String,
 
+    /// 当前系统
     pub platform: Platform,
 }
 
 #[napi(js_name = "getOsFileManagerPath")]
-pub fn get_os_file_manager_path_node() -> napi::Result<AppInfo> {
-    let info = unsafe {
-        let info = get_os_file_manager_path().map_err(|err| {
-            Error::new(
-                Status::GenericFailure,
-                format!("获取系统窗口信息失败:, {}", err),
-            )
-        })?;
+pub fn get_os_file_manager_path_node() -> Result<AppInfo> {
+    let info = unsafe { get_os_file_manager_path() }.map_err(|err| {
+        Error::new(
+            Status::GenericFailure,
+            format!("获取系统窗口信息失败: {}", err),
+        )
+    })?;
 
-        AppInfo {
-            hwnd_id: info.hwnd_id as u32,
-            title: info.title,
-            bundle_id: info.bundle_id,
-            is_active: info.is_active,
-            dir: info.dir,
-            exec: info.exec,
-            platform: Platform::from(info.platform),
-        }
-    };
-
-    Ok(info)
+    Ok(AppInfo {
+        hwnd_id: info.hwnd_id as u32,
+        title: info.title,
+        bundle_id: info.bundle_id,
+        is_active: info.is_active,
+        dir: info.dir,
+        exec: info.exec,
+        platform: Platform::from(info.platform),
+    })
 }
--- crates/binding/src/lib.rs
+++ crates/binding/src/lib.rs
@@ -1,12 +1,26 @@
-use std::env;
-use utils::env::is_debug;
-use utils::hook::panic_hook::register_panic_hook;
+use std::{env, panic::set_hook};
 
+use backtrace::Backtrace;
 pub mod explore;
 pub mod log;
+
 pub mod system_info;
 
-#[napi::module_init]
+pub fn is_debug() -> bool {
+    cfg!(debug_assertions)
+}
+
+pub fn register_panic_hook() {
+    // set_hook: 注册一个自定义 panic 钩子，替换以前注册的钩子，这个钩子将在线程调用 panic 之前被调用
+    // 钩子会收到一个 PanicInfo 结构，其中包含有关 panic 起源的信息，包括传递给 panic! 的 payload 和 panic 的来源代码位置。
+    set_hook(Box::new(|panic_info| {
+        let backtrace = Backtrace::new();
+        println!("panic 信息: {:#?}\n", panic_info,);
+        println!("调用栈:\n{:?}\n", backtrace);
+    }));
+}
+
+#[napi_derive::module_init]
 fn init() {
     if is_debug() || env::var("CUSTOM_DEBUG").unwrap_or_default() == "1" {
         register_panic_hook()
